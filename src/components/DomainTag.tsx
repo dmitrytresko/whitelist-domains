de6802853539fabcaa7634c82e0134c35ddd1301
dmitrytresko <dimatresko@gmail.com>
2025-04-01 20:57:42 +0200

    improve domain tag component

--- src/components/DomainTag.tsx
+++ src/components/DomainTag.tsx
@@ -3,13 +3,17 @@
 import React, { useState } from "react";
 import Image from "next/image";
 import RemoveDomainDialog from "./RemoveDomainDialog";
+import { DomainConfig } from "@/contexts/DomainContext";
 
 interface DomainTagProps {
-  label: string;
+  domain: DomainConfig;
   allowRemove?: boolean;
 }
 
-const DomainTag: React.FC<DomainTagProps> = ({ label, allowRemove }) => {
+const DomainTag: React.FC<DomainTagProps> = ({
+  domain,
+  allowRemove = true,
+}) => {
   const [isRemoveDialogOpen, setIsRemoveDialogOpen] = useState(false);
 
   return (
@@ -20,10 +24,18 @@ const DomainTag: React.FC<DomainTagProps> = ({ label, allowRemove }) => {
           ${allowRemove ? "pr-2" : "pr-3"}
         `}
       >
-        <span className="text-xs text-foreground-gray">{label}</span>
+        <span className="text-xs text-foreground-gray">
+          {domain.domainName}
+        </span>
+
+        {/* hides "X" button within each domain tag in this particular dialog on purpose, 
+          because logically user shouldn't be allowed to click there while already being in the Remove Domain dialog. 
+          Might be a slight inaccuracy from the design perspective 
+        */}
         {allowRemove && (
           <button
-            className="flex items-center justify-center p-1 rounded-full hover:bg-gray-200 active:bg-gray-200 transition-all cursor-pointer"
+            className="flex items-center justify-center p-1 rounded-full 
+            hover:bg-gray-200 active:bg-gray-200 transition-all cursor-pointer"
             type="button"
             onClick={() => setIsRemoveDialogOpen(true)}
           >
@@ -36,11 +48,12 @@ const DomainTag: React.FC<DomainTagProps> = ({ label, allowRemove }) => {
           </button>
         )}
       </div>
+
       {allowRemove && (
         <RemoveDomainDialog
           open={isRemoveDialogOpen}
           onClose={() => setIsRemoveDialogOpen(false)}
-          domainName={label}
+          domain={domain}
         />
       )}
     </>
